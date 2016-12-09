589d9a8a5c26b48b6ceead86a9354462656cf423
Agustin Prats <agustin.prats@intel.com>
2016-12-09 12:34:24 +0100

    improve background detection

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,11 @@ RUN stack clean
 RUN stack build
 
 # Instructions
+# docker rmi gotas-build -f
+# docker rmi codifilo/gotas:1.1.0 -f
+# docker stop gotas-build-cont
+# docker rm gotas-build-cont
+#
 # docker build -t gotas-build .
 # docker create --name gotas-build-cont gotas-build
 # docker cp gotas-build-cont:/opt/gotas/.stack-work/dist/x86_64-linux/Cabal-1.24.0.0/build/gotas/gotas ./docker-build/
--- src/ElTiempoEs.hs
+++ src/ElTiempoEs.hs
@@ -31,94 +31,91 @@ radarBounds = ImgBounds {
 
 -- transforms a pixel to precipitaion in mm/h
 pixelToMmh :: PixelRGB8 -> Precip
-pixelToMmh p@(PixelRGB8 r g b) =
-    if r == g && g == b
+pixelToMmh rgb =
+  let hsl@(HSL h s l) = toHSL rgb in
+    if s < 10.0
       then None
-      else  let distances = A.first (hueDistance p) <$> combinedLegend
+      else  let distances = A.first (hueDistance hsl) <$> combinedLegend
                 minDistance (d1, _) (d2, _) = compare d1 d2 in
                 snd $ L.minimumBy minDistance distances
 
--- yCbCrEuclidieanDistance :: PixelYCbCr8 -> PixelYCbCr8 -> Double
--- yCbCrEuclidieanDistance (PixelYCbCr8 y1 cb1 cr1) (PixelYCbCr8 y2 cb2 cr2) =
---       sqrt $ (fromIntegral y2 - fromIntegral y1)^2 + (fromIntegral cb2 - fromIntegral cb1)^2 + (fromIntegral cr2 - fromIntegral cr1)^2
---
--- -- deltaE94 CIELAB formula
--- deltaE94 :: PixelRGB8 -> PixelRGB8 -> Double
--- deltaE94 (PixelRGB8 r1 g1 bb1) (PixelRGB8 r2 g2 bb2) =
---     let (CIELAB l1 a1 b1) = fromRGB (RGB (toInteger r1) (toInteger g1) (toInteger bb1))
---         (CIELAB l2 a2 b2) = fromRGB (RGB (toInteger r2) (toInteger g2) (toInteger bb2))
---         dl = l1 - l2
---         c1 = sqrt $ a1^2 + b1^2
---         c2 = sqrt $ a2^2 + b2^2
---         dc = c1 - c2
---         da = a1 - a2
---         db = b1 - b2
---         sl = 1
---         sc = 1 + k1 * c1
---         sh = 1 + k2 * c1
---         kl = 1     -- graphic arts = 1; textiles = 2
---         k1 = 0.045 -- graphic arts = 0.045; textiles = 0.048
---         k2 = 0.015 -- graphic arts = 0.015; textiles = 0.014
---         kc = 1
---         kh = 1
---         dh = sqrt $ da^2 + db^2 - dc^2 in
---         sqrt $ (dl / kl*sl)^2 + (dc / kc*sc)^2 + (dh / kh*sh)
+hueDistance :: HSL -> HSL -> Double
+hueDistance (HSL h1 _ _) (HSL h2 _ _) = abs $ h1 - h2
 
-hueDistance :: PixelRGB8 -> PixelRGB8 -> Double
-hueDistance p1 p2 = abs $ hue p1 - hue p2
+data HSL = HSL {
+      hue        :: Double
+    , saturation :: Double
+    , lightness  :: Double
+} deriving (Eq, Ord, Show, Read)
 
-hue :: PixelRGB8 -> Double
-hue (PixelRGB8 r g b) = atan2 (sqrt 3 * (fromIntegral g - fromIntegral b)) (2 * fromIntegral r - fromIntegral g - fromIntegral b)
+toHSL :: PixelRGB8 -> HSL
+toHSL (PixelRGB8 r g b) =
+  let r' = fromIntegral r / 255.0
+      g' = fromIntegral g / 255.0
+      b' = fromIntegral b / 255.0
+      max' = max r' $ max g' b'
+      min' = min r' $ min g' b'
+      l = (max' + min') / 2.0
+      d = max' - min'
+      s | max' == min' = 0.0
+        | l > 0.5 = d / (2 - max' - min')
+        | otherwise = d / (max' + min')
+      h | max' == min' = 0.0
+        | max' == r' = (g' - b') / d + (if g < b then 6 else 0)
+        | max' == g' = (b' - r') / d + 2
+        | max' == b' = (r' - g') / d + 4
+        | otherwise = 0.0 in
+      HSL (h*100+0.5) (s*100+0.5) (l*100+0.5)
 
-combinedLegend :: [(PixelRGB8, Precip)]
+combinedLegend :: [(HSL, Precip)]
 combinedLegend = rainLegend ++ snowLegend ++ mixedLegend
 
-rainLegend :: [(PixelRGB8, Precip)]
+rainLegend :: [(HSL, Precip)]
 rainLegend = [
-      (PixelRGB8 100 200   0, Rain   0.1)
-    , (PixelRGB8  80 175   0, Rain   0.3)
-    , (PixelRGB8  60 150   0, Rain   0.5)
-    , (PixelRGB8  40 125   0, Rain   0.9)
-    , (PixelRGB8  20 105   0, Rain   1.5)
-    , (PixelRGB8 250 250   0, Rain   2.4)
-    , (PixelRGB8 250 175   0, Rain   3.6)
-    , (PixelRGB8 250 100   0, Rain  10.0)
-    , (PixelRGB8 250   0   0, Rain  20.0)
-    , (PixelRGB8 200   0  25, Rain  49.0)
-    , (PixelRGB8 150   0  75, Rain 100.0)
-    , (PixelRGB8 150   0 150, Rain 205.0)
-    , (PixelRGB8 150  75 200, Rain 421.0)
-    , (PixelRGB8 200 150 200, Rain 865.0)
+      (toHSL $ PixelRGB8 100 200   0, Rain   0.1)
+    , (toHSL $ PixelRGB8  80 175   0, Rain   0.3)
+    , (toHSL $ PixelRGB8  60 150   0, Rain   0.5)
+    , (toHSL $ PixelRGB8  40 125   0, Rain   0.9)
+    , (toHSL $ PixelRGB8  20 105   0, Rain   1.5)
+    , (toHSL $ PixelRGB8 250 250   0, Rain   2.4)
+    , (toHSL $ PixelRGB8 250 175   0, Rain   3.6)
+    , (toHSL $ PixelRGB8 250 100   0, Rain  10.0)
+    , (toHSL $ PixelRGB8 250   0   0, Rain  20.0)
+    , (toHSL $ PixelRGB8 200   0  25, Rain  49.0)
+    , (toHSL $ PixelRGB8 150   0  75, Rain 100.0)
+    , (toHSL $ PixelRGB8 150   0 150, Rain 205.0)
+    , (toHSL $ PixelRGB8 150  75 200, Rain 421.0)
+    , (toHSL $ PixelRGB8 200 150 200, Rain 865.0)
   ]
 
-snowLegend :: [(PixelRGB8, Precip)]
+snowLegend :: [(HSL, Precip)]
 snowLegend = [
-      (PixelRGB8 210 210 250, Snow 0.061)
-    , (PixelRGB8 180 180 250, Snow 0.1  )
-    , (PixelRGB8 150 150 250, Snow 0.15 )
-    , (PixelRGB8 120 120 250, Snow 0.2  )
-    , (PixelRGB8  90  90 250, Snow 0.3  )
-    , (PixelRGB8  60  60 250, Snow 0.5  )
-    , (PixelRGB8  30  30 210, Snow 0.7  )
-    , (PixelRGB8   0   0 180, Snow 0.9  )
-    , (PixelRGB8   0   0 150, Snow 2.0  )
-    , (PixelRGB8   0   0 120, Snow 3.3  )
+      (toHSL $ PixelRGB8 210 210 250, Snow 0.061)
+    , (toHSL $ PixelRGB8 180 180 250, Snow 0.1  )
+    , (toHSL $ PixelRGB8 150 150 250, Snow 0.15 )
+    , (toHSL $ PixelRGB8 120 120 250, Snow 0.2  )
+    , (toHSL $ PixelRGB8  90  90 250, Snow 0.3  )
+    , (toHSL $ PixelRGB8  60  60 250, Snow 0.5  )
+    , (toHSL $ PixelRGB8  30  30 210, Snow 0.7  )
+    , (toHSL $ PixelRGB8   0   0 180, Snow 0.9  )
+    , (toHSL $ PixelRGB8   0   0 150, Snow 2.0  )
+    , (toHSL $ PixelRGB8   0   0 120, Snow 3.3  )
   ]
 
-mixedLegend :: [(PixelRGB8, Precip)]
+mixedLegend :: [(HSL, Precip)]
 mixedLegend = [
-      (PixelRGB8 250 225 225, Mixed   0.07)
-    , (PixelRGB8 250 200 205, Mixed   0.1 )
-    , (PixelRGB8 250 175 185, Mixed   0.3 )
-    , (PixelRGB8 250 150 165, Mixed   0.5 )
-    , (PixelRGB8 250 100 125, Mixed   1.5 )
-    , (PixelRGB8 250  75 105, Mixed   2.3 )
-    , (PixelRGB8 250  50  85, Mixed   3.6 )
-    , (PixelRGB8 225  25  65, Mixed  10.0 )
-    , (PixelRGB8 225  25  65, Mixed  10.0 )
-    , (PixelRGB8 200   0  45, Mixed  20.0 )
-    , (PixelRGB8 175   0  25, Mixed  49.0 )
-    , (PixelRGB8 150   0   0, Mixed 100.0 )
+      (toHSL $ PixelRGB8 250 225 225, Mixed   0.07)
+    , (toHSL $ PixelRGB8 250 200 205, Mixed   0.1 )
+    , (toHSL $ PixelRGB8 250 175 185, Mixed   0.3 )
+    , (toHSL $ PixelRGB8 250 150 165, Mixed   0.5 )
+    , (toHSL $ PixelRGB8 250 100 125, Mixed   1.5 )
+    , (toHSL $ PixelRGB8 250  75 105, Mixed   2.3 )
+    , (toHSL $ PixelRGB8 250  50  85, Mixed   3.6 )
+    , (toHSL $ PixelRGB8 225  25  65, Mixed  10.0 )
+    , (toHSL $ PixelRGB8 225  25  65, Mixed  10.0 )
+    , (toHSL $ PixelRGB8 200   0  45, Mixed  20.0 )
+    , (toHSL $ PixelRGB8 175   0  25, Mixed  49.0 )
+    , (toHSL $ PixelRGB8 150   0   0, Mixed 100.0 )
   ]
 
 -- Radar URL generation
